0d5fda61ec7ce6acdecde8677ef127fc1162c992
Jeslin1999 <jeslinelizabeth8606@gmail.com>
2024-09-09 10:21:42 +0530

    viewedprofile

--- dating_app/settings.py
+++ dating_app/settings.py
@@ -78,23 +78,10 @@ WSGI_APPLICATION = 'dating_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'datingbackend',
-        'USER': 'root',
-        'PASSWORD':'',
-        'HOST':'localhost',
-        'PORT':'3306'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
